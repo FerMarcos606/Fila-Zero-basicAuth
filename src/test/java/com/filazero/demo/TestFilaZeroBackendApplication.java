4e2c7020172df2d39a2655f7dee517fc82640c14
Fer <desarrollowebfer@gmail.com>
2025-11-01 08:35:07 +0100

    DB fix: udate files

--- src/test/java/com/filazero/demo/TestFilaZeroBackendApplication.java
+++ src/test/java/com/filazero/demo/TestFilaZeroBackendApplication.java
@@ -1,11 +1,11 @@
 package com.filazero.demo;
 
-import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.SpringApplication;
 
 public class TestFilaZeroBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(FilaZeroBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+		// SpringApplication.from(FilaZeroBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
--- src/test/java/com/filazero/demo/products/ProductControllerTest.java
+++ src/test/java/com/filazero/demo/products/ProductControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 
